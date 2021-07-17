eaf76781e9a5c2ed88616367eba4e70f2f9bd158
Omkar75 <omkarzagade75@gmail.com>
2021-07-17 18:03:55 +0530

    1 Deploy

---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const projects = [
+  {
+    title: "xFlix",
+    subtitle: "MERN Stack",
+    Role : "Role : Full-Stack Developer", 
+    description: "Tired Of YouTube recommending videos that waste your time. Try this Video Streaming web-app that feeds on youtube but shows only what you store!!",
+    image: "./xflix.PNG",
+    link: "https://xflixoz.netlify.app",
+  },
+  {
+    title: "QKart",
+    subtitle: "MERN Stack",
+    Role : "Role : Full-Stack Developer",
+    description:"E-commerce store to fulfill customers day to day need. Now Buy your stuff while laying on the Bed!",
+    image: "./qkart.PNG",
+    link: "https://qkart-frontendoz.netlify.app/",
+  },
+  {
+    title: "xBoard",
+    subtitle: "JavaScript",
+    Role: "Role : Frontend Developer",
+    description:
+      "Responsive Front-end News Feed site, with Beautifull UX design. News are fetched by API's of flipboard",
+    image: "./xboard.PNG",
+    link: "https://xboardoz.netlify.app",
+  },
+  {
+    title: "QTrip",
+    subtitle: "JavaScript",
+    Role: "Role : Frontend Developer",
+    description:
+      "Visit the Place You have always Dreamed of, with exicting adventures and fun. A Travel Booking site which will help user not only to book cities but also the adventures that city is famous for.",
+    image: "./qtrip.PNG",
+    link: "https://qtrip-dynamicoz.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Edu-Tech Company based on idealogy of 'Learn By Doing'. Train students to Build Industry Products from scratch to Deployment.",
+    image: "https://photos.angel.co/startups/i/6691484-3bdfc15d3aab360d8462695101133b57-medium_jpg.jpg?buster=1546587025",
+    name: "Full-Stack Developer",
+    company: "Crio.do",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node",
+  "Expree.js",
+  "MongoDB",
+  "Material UI",
+];
